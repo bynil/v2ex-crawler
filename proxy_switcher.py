88c4427c6f6b66a74462a3f73934bb2324ad27aa
gexiao <gxgexiao@gmail.com>
2024-04-08 00:47:09 +0800

    Decrease waiting time

--- proxy_switcher.py
+++ proxy_switcher.py
@@ -125,7 +125,7 @@ def random_proxy():
     if str(proxy) in random_proxy_mute_dict:
         mute_time = random_proxy_mute_dict[str(proxy)]
         if mute_time > time.time():
-            time.sleep(10)
+            time.sleep(1)
             return random_proxy()
         else:
             del random_proxy_mute_dict[str(proxy)]
